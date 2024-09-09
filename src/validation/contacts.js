673459307c438b032bb99d73c64b807e470fdfba
Alina-ps <alinaserg2014@gmail.com>
2024-09-09 22:19:00 +0200

    step 2 validation

---
+++ src/validation/contacts.js
@@ -0,0 +1,36 @@
+import Joi from 'joi';
+
+export const createContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20).required().message({
+    'string.base': 'Username should be a string',
+    'string.min': 'Username should have at least {#limit} characters',
+    'string.max': 'Username should have at most {#limit} characters',
+    'any.required': 'Username is required',
+  }),
+  phoneNumber: Joi.string().min(3).max(20).required(),
+  email: Joi.string().email().min(3).max(20),
+  isFavourite: Joi.boolean().default('false'),
+  contactType: Joi.string()
+    .min(3)
+    .max(20)
+    .valid('work', 'home', 'personal')
+    .default('personal')
+    .required(),
+});
+
+export const updateContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20).message({
+    'string.base': 'Username should be a string',
+    'string.min': 'Username should have at least {#limit} characters',
+    'string.max': 'Username should have at most {#limit} characters',
+    'any.required': 'Username is required',
+  }),
+  phoneNumber: Joi.string().min(3).max(20),
+  email: Joi.string().email().min(3).max(20),
+  isFavourite: Joi.boolean().default('false'),
+  contactType: Joi.string()
+    .min(3)
+    .max(20)
+    .valid('work', 'home', 'personal')
+    .default('personal'),
+});
