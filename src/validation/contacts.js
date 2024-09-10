4fabc9e2c362e27f87ec912f9becd90f276f9a95
Alina-ps <alinaserg2014@gmail.com>
2024-09-10 20:06:00 +0200

    upd

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,12 +1,7 @@
 import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
-  name: Joi.string().min(3).max(20).required().messages({
-    'string.base': 'Username should be a string',
-    'string.min': 'Username should have at least {#limit} characters',
-    'string.max': 'Username should have at most {#limit} characters',
-    'any.required': 'Username is required',
-  }),
+  name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().min(3).max(20).required(),
   email: Joi.string().email().min(3).max(20),
   isFavourite: Joi.boolean().default(false),
@@ -19,11 +14,7 @@ export const createContactSchema = Joi.object({
 });
 
 export const updateContactSchema = Joi.object({
-  name: Joi.string().min(3).max(20).message({
-    'string.base': 'Username should be a string',
-    'string.min': 'Username should have at least {#limit} characters',
-    'string.max': 'Username should have at most {#limit} characters',
-  }),
+  name: Joi.string().min(3).max(20),
   phoneNumber: Joi.string().min(3).max(20),
   email: Joi.string().email().min(3).max(20),
   isFavourite: Joi.boolean().default('false'),
