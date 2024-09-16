bf27324cef11a3b9760c1910f33de8c1977eafdd
Alina-ps <alinaserg2014@gmail.com>
2024-09-16 20:00:32 +0200

    upd

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -1,10 +1,17 @@
-import { model, Schema } from 'mongoose';
+import mongoose, { model, Schema } from 'mongoose';
 
 const usersSchema = new Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      unique: true,
+      default: function () {
+        return this._id;
+      },
+    },
   },
   { timestamps: true, versionKey: false },
 );
